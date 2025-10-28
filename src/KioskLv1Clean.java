fe3a4d5d2d8ce5441729b42fa004be5669805034
wnel2346 <wnel2346@gmail.com>
2025-10-28 18:08:11 +0900

    레벨1 90%클리어

---
+++ src/KioskLv1Clean.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class KioskLv1Clean {
+    public static void main(String[] args){
+
+        Scanner scanner=new Scanner(System.in);
+        int menuNumber = -1;
+        boolean isRunning = true;
+
+        while(isRunning){
+            System.out.println("[BURGERKING MENU ]");
+            System.out.println("1.크리스피 붉닭 치킨 |W5.9|치킨패티,불닭소스,베이컨,양파,양상추가들어간치킨버거");
+            System.out.println("2.트러플 머쉬룸 와퍼 |W8.5|소고기패티,각종버섯,양파,토마토,양상추,트러플소스가토핑된버거");
+            System.out.println("3.");
+        }
+
+
+    }
+}
