cec32fb1b92180cac5f7314bb64ea6c0793dd28d
wnel2346 <wnel2346@gmail.com>
2025-10-30 12:31:23 +0900

    3단계까지완성

--- src/KioskLv1Clean.java
+++ src/KioskLv1Clean.java
@@ -13,6 +13,23 @@ public class KioskLv1Clean {
             System.out.println("2.트러플 머쉬룸 와퍼 |W8.5|소고기패티,각종버섯,양파,토마토,양상추,트러플소스가토핑된버거");
             System.out.println("3.딥 프러플 프라이 |W2.9|감자튀김에 프러틀소스를부운거");
             System.out.println("0.종료");
+            System.out.println("메뉴를 선택하세요 (숫자 입력): ");
+
+            if (scanner.hasNextInt()){
+                menuNumber = scanner.nextInt();
+
+                if (menuNumber ==0){
+                    System.out.println("프로그램을 종료합니다.");
+                    isRunning = false;
+                }else if (menuNumber ==1 ||menuNumber ==2 ||menuNumber ==3){
+                    System.out.println(menuNumber+"잘못된 메뉴 번호입니다. 다시 선택해주세요.");
+                }else{
+                    System.out.println("유효하지 않은 입력입니다. 숫자를 입력해주세요.");
+                    scanner.next();
+                }
+                System.out.println("------------------------------------");
+            }
+            scanner.close();
         }
 
 
